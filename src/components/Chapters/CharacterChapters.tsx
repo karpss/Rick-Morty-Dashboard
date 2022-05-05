ce1ff510e085669ce1796710a20f9bb06a21b3ff
karpss <ola.fam10@gmail.com>
2022-05-05 14:29:20 +0100

    chapter page styled

--- src/components/Chapters/CharacterChapters.tsx
+++ src/components/Chapters/CharacterChapters.tsx
@@ -4,17 +4,19 @@ import { CharacterChapterProps, EpisodeProps } from '../types';
 
 const CharacterChapters: React.FC<CharacterChapterProps> = ({episodes,closePage}) => {
   return (
-    <div>
-      <button onClick={closePage}>
+    <div className="chapterContainer">
+      <div className="chapterBody">
+      <button className="chapterClose" onClick={closePage}>
         X
       </button>
       <h2>Chapters Featured</h2>
-      <div>
+      <div className="characterInfo--text">
         {episodes.map((episode: EpisodeProps) => (
             <p key={episode.id} >
-              {episode.episode} - <span>{episode.name}</span>,
+              {episode.episode} - <span>{episode.name}</span>
             </p>
           ))}
+          </div>
       </div>
       </div>
   )
