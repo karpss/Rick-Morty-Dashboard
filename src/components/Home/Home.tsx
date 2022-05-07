2ccc7965f6e50373dff18c13d8b35bbcac62ced2
karpss <ola.fam10@gmail.com>
2022-05-07 08:22:54 +0100

    solved git commit issues

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,30 +1,30 @@
-//* eslint-disable */
 import React from 'react';
 import useFetchCharacterDetails from '../../hooks/useFetchCharacterDetails';
 import CharacterDetails from '../Characters/CharacterDetails';
+import LoadingScreen from '../Loading/LoadingScreen';
 
 const Home: React.FC = () => {
   const {characterDetails} = useFetchCharacterDetails();
   const renderCharacterDetails = (): any => {
-    if (!characterDetails.length) {
-      return <div>Character Details Loading</div>;
+    if (characterDetails.length > 0) {
+      return characterDetails.map((char: any) => {
+        const {id, name, gender, species, image, origin, location, episode} =
+          char;
+        return (
+          <CharacterDetails
+            key={id}
+            name={name}
+            gender={gender}
+            species={species}
+            image={image}
+            origin={origin}
+            location={location}
+            episode={episode}
+          />
+        );
+      });
     }
-    return characterDetails.map((char: any) => {
-      const {id, name, gender, species, image, origin, location, episode} =
-        char;
-      return (
-        <CharacterDetails
-          key={id}
-          name={name}
-          gender={gender}
-          species={species}
-          image={image}
-          origin={origin}
-          location={location}
-          episode={episode}
-        />
-      );
-    });
+    return <LoadingScreen />;
   };
 
   return (
